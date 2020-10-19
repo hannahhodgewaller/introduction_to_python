4933b71d3103169ded4ba76bc28ca4252767027d
hannahhodgewaller <71869617+hannahhodgewaller@users.noreply.github.com>
2020-10-19 11:59:57 +0100

    Add files via upload

---
+++ solutions/chapter_4/animal_filter_two.py
@@ -0,0 +1,8 @@
+﻿# Filter the animals DataFrame:
+
+# Filter the animals DataFrame where the AnimalClass was Mammal or the PumpHoursTotal was greater than 8.
+
+
+mammals_pumphours = animals[(animals['AnimalClass'] == "Mammal") | (animals['PumpHoursTotal'] > 8)]
+
+mammals_pumphours.head()
