7db4ec27e883d3f729bc0644a0901c4663524993
hannahhodgewaller <71869617+hannahhodgewaller@users.noreply.github.com>
2021-08-18 09:30:28 +0100

    Add files via upload

--- solutions/chapter_3/read_excel.py
+++ solutions/chapter_3/read_excel.py
@@ -1,9 +1,9 @@
 # Load in the police data
 
-police = pd.read_excel("../data/police_data.xlsx", sheetname=1)
+police = pd.read_excel("../data/police_data.xlsx", sheet_name=1)
 
 police.head()
 
-# The paramater sheetname = 1 imports the second sheet
-# sheetname = "Table P1" will also work
-# Note from Pandas 0.21.0 use sheet_name = 
+# The paramater sheet_name = 1 imports the second sheet
+# sheet_name = "Table P1" will also work
+# Note if you're using an older version of Pandas ( < 0.21.0 ) the parameter is sheetname = with no space. 
